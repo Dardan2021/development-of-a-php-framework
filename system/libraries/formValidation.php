5e41c7731ae9dca7e6ed7747d238d8a90e69a627
Dardan2021 <78737429+Dardan2021@users.noreply.github.com>
2021-08-14 12:26:45 +0200

    added other helpers and validation library

--- system/libraries/formValidation.php
+++ system/libraries/formValidation.php
@@ -1,50 +1,50 @@
 <?php
 
-    trait formValidation
-    {
-        public static $error = array();
+trait formValidation
+{
+  public static $error = array();
 
-        public static function validation($fieldName, $label, $rules)
-        {
-            if ($_SERVER['REQUEST_METHOD'] == 'post' || $_SERVER['REQUEST_METHOD'] == 'POST')
-            {
-                $data = $_POST[$fieldName];
-            }
-            else
-            {
-                echo "sorry it is not found";
-            }
+  public static function validation($fieldName, $label, $rules)
+  {
+      if ($_SERVER['REQUEST_METHOD'] == 'post' || $_SERVER['REQUEST_METHOD'] == 'POST')
+      {
+          $data = $_POST[$fieldName];
+      }
+      else
+      {
+          echo "sorry it is not found";
+      }
 
-            $pattren = "/^[a-zA-Z]+$/";
-            $rules = explode("|", $rules);
+      $pattren = "/^[a-zA-Z]+$/";
+      $rules = explode("|", $rules);
 
-            if(in_array("required", $rules))
-            {
-                if(empty($data))
-                {
-                    return self::$error[$fieldName] = $label. " is required";
-                }
-            }
-            else if(in_array("not_int", $rules))
-            {
-                if(!preg_match($pattren, $data))
-                {
-                    return self::$error[$fieldName] = $label. " must have only alphabet";
-                }
-            }
-        }
+      if(in_array("required", $rules))
+      {
+          if(empty($data))
+          {
+              return self::$error[$fieldName] = $label. " is required";
+          }
+      }
+      else if(in_array("not_int", $rules))
+      {
+          if(!preg_match($pattren, $data))
+          {
+              return self::$error[$fieldName] = $label. " must have only alphabet";
+          }
+      }
+  }
 
-        public static function run()
-        {
-            if(empty(self::$error))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
+  public static function run()
+  {
+      if(empty(self::$error))
+      {
+          return true;
+      }
+      else
+      {
+          return false;
+      }
+  }
+}
 
 ?>
